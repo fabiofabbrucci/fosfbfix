6f78474ab019fa088b7a2dc90b798fe3b276fd20
Fabio Fabbrucci <fabio.fabbrucci@gmail.com>
2012-02-15 19:19:14 +0100

    generated entity user

---
+++ src/FFF/UserBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FFF\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * FFF\UserBundle\Entity\User
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class User
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
