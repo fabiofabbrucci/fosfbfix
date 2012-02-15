78f59a1e3dfaeb6295333b916ec2dbe31d81f879
Fabio Fabbrucci <fabio.fabbrucci@gmail.com>
2012-02-15 19:16:53 +0100

    removed from appkernel fos (cause throw an error) created a bundle FFF/user

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
+            //new FOS\UserBundle\FOSUserBundle(),
+            new FFF\UserBundle\FFFUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/FFF/UserBundle/FFFUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FFF\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FFFUserBundle extends Bundle
+{
+}
