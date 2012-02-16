33709da01fa1a78199be018ca64f9b29e50378c2
Fabio Fabbrucci <fabio.fabbrucci@gmail.com>
2012-02-16 10:16:29 +0100

    chain of providers and overwrite of login method

--- src/FFF/UserBundle/Controller/DefaultController.php
+++ src/FFF/UserBundle/Controller/DefaultController.php
@@ -16,4 +16,48 @@ class DefaultController extends Controller
     {
         return array('name' => $name);
     }
+    
+    /**
+     * Finds and displays a Page entity.
+     *
+     * @Route("/login")
+     * @Template
+     *
+     *
+     * */
+    public function loginAction() {
+        $request = $this->container->get('request');
+        $session = $request->getSession();
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
+            $error = $error->getMessage();
+        }
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
+
+        return array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+        );
+    }
+
+    /**
+     * Dummy controller.
+     *
+     * @Route("/loginFb")
+     * @Template
+     */
+    public function loginFbAction() {
+    }
 }
