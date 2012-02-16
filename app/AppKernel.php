b530c8b3def6138df9d826df1be474beb6fa4f01
Fabio Fabbrucci <fabio.fabbrucci@gmail.com>
2012-02-16 09:34:12 +0100

    Added FOSFacebookBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new FFF\UserBundle\FFFUserBundle(),
+            new FOS\FacebookBundle\FOSFacebookBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
