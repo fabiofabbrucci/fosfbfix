2603f3f5a92bc10fab378c02f059d02ab3a82f3d
Fabio Fabbrucci <fabio.fabbrucci@gmail.com>
2012-02-16 10:56:45 +0100

    fix vittore

--- src/FFF/UserBundle/Controller/DefaultController.php
+++ src/FFF/UserBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace FFF\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 class DefaultController extends Controller
 {
